a0ae2b8f13bedb8651ed5bae7372429c6195dc21
yasuhiro.kato <yasuhiro.kato@cct-inc.co.jp>
2018-12-05 22:04:28 +0900

    Add INTERNET permisson to manifest file

--- app/src/main/java/tokyo/ysbrothersk/ake/ui/main/MainFragment.kt
+++ app/src/main/java/tokyo/ysbrothersk/ake/ui/main/MainFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import org.jsoup.Jsoup
 import tokyo.ysbrothersk.ake.R
 
 class MainFragment : Fragment() {
@@ -32,6 +33,10 @@ class MainFragment : Fragment() {
 
         // 1. 気象庁から東京の天気情報取得
         // 2. 東京の服装指数を取得
+        GlobalScope.launch {
+            val doc = Jsoup.connect("https://tenki.jp/indexes/dress/3/16/4410/").get()
+            println(doc.title())
+        }
 
         // 非同期で実行後、awaitで取得できたことを確認したらfragmentに表示する
     }
